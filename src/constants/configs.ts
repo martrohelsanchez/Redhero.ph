c234cc58ca8bde68ce87c3d3f59acb978bcd7c7a
Mart Rohel Sanchez <mart@symph.co>
2021-03-21 14:30:51 +0800

    🔧Configure GridThemeProvider

---
+++ src/constants/configs.ts
@@ -0,0 +1,28 @@
+export const GRID_THEME = {
+  gridColumns: 12,
+  breakpoints: {
+    xxl: 1440,
+    xl: 1200,
+    lg: 992,
+    md: 768,
+    sm: 576,
+    xs: 575,
+  },
+  row: {
+    padding: 15,
+  },
+  col: {
+    padding: 15,
+  },
+  container: {
+    padding: 15,
+    maxWidth: {
+      xxl: 1441,
+      xl: 1440,
+      lg: 960,
+      md: 720,
+      sm: 540,
+      xs: 540,
+    },
+  },
+};
