2cf286dc778ec297fa63c3a1add3cb4988950085
Mart Rohel Sanchez <mart@symph.co>
2021-03-22 09:04:08 +0800

    💄Ensure consistent styling in all browsers by resetting css using styled-reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Reset } from 'styled-reset';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root"),
+  <React.StrictMode>
+    <Reset />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
