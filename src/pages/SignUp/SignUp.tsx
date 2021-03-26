f60ec7764d2f0518d1c063ec78ffb18c3282e8e7
Mart Rohel Sanchez <mart@symph.co>
2021-03-26 09:47:15 +0800

    🔧Sign up using signInWithRedirect

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -22,7 +22,7 @@ function SignUp(props: RouteComponentProps) {
 
   async function signUpWithGoggle() {
     try {
-      await auth.signInWithPopup(googleProvider);
+      await auth.signInWithRedirect(googleProvider);
 
       history.push(routes.USER_INFO_1);
     } catch (e) {
